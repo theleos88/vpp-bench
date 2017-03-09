95f966682cef73a512b00cb413987f153628e03e
Leonardo Linguaglossa <theleos88@gmail.com>
2017-03-09 16:45:53 +0100

    new version of the script

--- scripts/start-vpp-xconnect.sh
+++ scripts/start-vpp-xconnect.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
+
+echo "Preparing path"
+cd $VPP_ROOT
+export STARTUP_CONF=`pwd`/startup.conf
+sudo env PATH=$PATH:$VPP_ROOT/build-root/install-vpp-native/vpp/bin
+
 if [[ $# -eq 0 ]] ; then
     echo 'STARTING WITH DEFAULT PARAMETERS. For next Usage:'
     echo './start-vpp-xconnect <prefix> LCxPy LCwPz'
     echo 'Change x, y, w, and z to match your NIC requirements. Do not use $'
     sleep 1
 
-    sudo $VPP_ROOT/build-root/build-vpp_debug-native/vpp/bin/vpp api-segment { prefix vpp gid vpp } dpdk { dev $LC2P1 dev $LC2P2 socket-mem 1024,1024 }
-    sudo $VPP_ROOT/build-root/install-vpp_debug-native/vpp/bin/vppctl -p vpp set int l2 xconnect $NAMELC2P2 $NAMELC2P1
-    sudo $VPP_ROOT/build-root/install-vpp_debug-native/vpp/bin/vppctl -p vpp set int state $NAMELC2P1 up
-    sudo $VPP_ROOT/build-root/install-vpp_debug-native/vpp/bin/vppctl -p vpp set int state $NAMELC2P2 up
+    make run-release
+    #sudo $VPP_ROOT/build-root/build-vpp-native/vpp/bin/vpp api-segment { prefix vpp gid vpp } dpdk { dev $LC2P1 dev $LC2P2 socket-mem 1024,1024 }
+    sudo $VPP_ROOT/build-root/install-vpp-native/vpp/bin/vppctl -p vpp set int l2 xconnect $NAMELC2P2 $NAMELC2P1
+    sudo $VPP_ROOT/build-root/install-vpp-native/vpp/bin/vppctl -p vpp set int state $NAMELC2P1 up
+    sudo $VPP_ROOT/build-root/install-vpp-native/vpp/bin/vppctl -p vpp set int state $NAMELC2P2 up
 
     exit 1
 fi
@@ -32,16 +39,16 @@ var="NAME"$3
 name2=${!var}
 
 echo "Starting vpp, $pci1, $pci2, $name1, $name2"
-sudo $VPP_ROOT/build-root/build-vpp_debug-native/vpp/bin/vpp api-segment { prefix $1 gid vpp } dpdk { dev $pci1 dev $pci2 socket-mem 1024,1024 }
-sleep 4
+make run-release
+#sudo $VPP_ROOT/build-root/install-vpp-native/vpp/bin/vpp api-segment { prefix $1 gid vpp } dpdk { dev $pci1 dev $pci2 socket-mem 1024,1024 }
+sleep 2
 
 echo "Crossconnecting, $name2 -> $name1"
-sudo $VPP_ROOT/build-root/install-vpp_debug-native/vpp/bin/vppctl -p $1 set int l2 xconnect $name2 $name1
-sudo $VPP_ROOT/build-root/install-vpp_debug-native/vpp/bin/vppctl -p $1 set int state $name1 up
-sudo $VPP_ROOT/build-root/install-vpp_debug-native/vpp/bin/vppctl -p $1 set int state $name2 up
+sudo $VPP_ROOT/build-root/install-vpp-native/vpp/bin/vppctl -p $1 set int l2 xconnect $name2 $name1
+sudo $VPP_ROOT/build-root/install-vpp-native/vpp/bin/vppctl -p $1 set int state $name1 up
+sudo $VPP_ROOT/build-root/install-vpp-native/vpp/bin/vppctl -p $1 set int state $name2 up
 
 if [ -n "$debug" ]; then
     echo "Not working"
     #sudo gdb -p `ps -ef | grep vpp_main | awk '{printf $2}'`
 fi
-
