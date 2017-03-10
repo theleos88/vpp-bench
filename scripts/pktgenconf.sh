5bb14ee0e730c7466cf85d13efba26496c2b7cec
Leonardo Linguaglossa <theleos88@gmail.com>
2017-03-10 18:40:29 +0100

    Pktgen configuration and VPP IP setup through linecard script + debug2

--- scripts/pktgenconf.sh
+++ scripts/pktgenconf.sh
@@ -3,7 +3,7 @@
 F=/tmp/pktgen.pkt
 
 echo "set ip src 1 $IPLC1P2/24"  > $F
-echo "set ip dst 1 $IPLC1P1/24" >> $F
+echo "set ip dst 1 $IPLC1P1" >> $F
 echo "set mac 1 $MACLC2P2" >> $F
 
 echo "set ip src 0 $IPLC1P1/24" >> $F
