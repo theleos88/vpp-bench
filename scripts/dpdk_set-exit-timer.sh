a1ceb6534168c59bdf08e2566691acfd4b0e840d
Leonardo Linguaglossa <theleos88@gmail.com>
2017-04-01 16:25:32 +0200

    dpdk execution timer

--- scripts/dpdk_set-exit-timer.sh
+++ scripts/dpdk_set-exit-timer.sh
@@ -2,13 +2,23 @@
 
 FILE=$RTE_PKTGEN/app/pktgen-main.c
 
+# Display usage
+usage (){
+    script=$(basename $0)
+    echo "This script adds an exit timer to dpdk."
+    echo "Usage:"
+    echo "./$script <activate [sleep-time] | deactivate>"
+    echo ""
+}
+
+# Check if no parameter
 if [[ $# -eq 0 ]] ; then
-    echo 'Error, no param provided. Usage:'
-    echo './activate-alarm.sh <activate [sleep-time] |deactivate>'
+    echo 'Error, no param provided.'
+    usage
     exit 1
 fi
 
-
+# Check if activate with custom value
 if [ $1 = "activate" ]; then
 	echo "Activating alarm"
 	sed -i '/define ALARM/c\#define ALARM 1' $FILE
