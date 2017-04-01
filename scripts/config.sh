a410d6364c2f4abfeec2843f4c035a71bba2cde2
Leonardo Linguaglossa <theleos88@gmail.com>
2017-04-01 15:57:05 +0200

    Update config.sh. Fixing all dependencies

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 
 # Linecards
-export LC1P1=0000:0b:00.0
-export LC1P2=0000:0b:00.1
-export LC2P1=0000:84:00.0
-export LC2P2=0000:84:00.1
+export LC0P0=0000:0b:00.0
+export LC0P1=0000:0b:00.1
+export LC1P0=0000:84:00.0
+export LC1P1=0000:84:00.1
 
 # Router-friendly Names
-export NAMELC1P1="TenGigabitEthernetb/0/0"
-export NAMELC1P2="TenGigabitEthernetb/0/1"
-export NAMELC2P1="TenGigabitEthernet84/0/0"
-export NAMELC2P2="TenGigabitEthernet84/0/1"
+export NAMELC0P0="TenGigabitEthernetb/0/0"
+export NAMELC0P1="TenGigabitEthernetb/0/1"
+export NAMELC1P0="TenGigabitEthernet84/0/0"
+export NAMELC1P1="TenGigabitEthernet84/0/1"
 
 # MAC addresses
-export MACLC1P1="90:e2:ba:cb:f5:38"
-export MACLC1P2="90:e2:ba:cb:f5:39"
-export MACLC2P1="90:e2:ba:cb:f5:44"
-export MACLC2P2="90:e2:ba:cb:f5:45"
+export MACLC0P0="90:e2:ba:cb:f5:38"
+export MACLC0P1="90:e2:ba:cb:f5:39"
+export MACLC1P0="90:e2:ba:cb:f5:44"
+export MACLC1P1="90:e2:ba:cb:f5:45"
 
-export DEVLC1P1="enp11s0f0"
-export DEVLC1P2="enp11s0f1"
-export DEVLC2P1="enp132s0f0"
-export DEVLC2P2="enp132s0f1"
+export DEVLC0P0="enp11s0f0"
+export DEVLC0P1="enp11s0f1"
+export DEVLC1P0="enp132s0f0"
+export DEVLC1P1="enp132s0f1"
 
 # IP addresses
-export IPLC1P1="1.1.1.11"
-export IPLC1P2="1.1.1.12"
-export IPLC2P1="1.1.1.21"
-export IPLC2P2="1.1.1.22"
+export IPLC0P0="1.1.1.11"
+export IPLC0P1="1.1.1.12"
+export IPLC1P0="1.1.1.21"
+export IPLC1P1="1.1.1.22"
 
 # VPP
 export VPP_ROOT=/usr/local/src/vpp
@@ -46,8 +46,8 @@ export PLUGS="$VPP_ROOT/build-root/install-vpp-native/vpp/lib64/vpp_plugins"
 export SFLAG="env PATH=$PATH:$BINS"
 
 # Aliases
-alias update-conf="svn export https://github.com/TeamRossi/vpp_dev/trunk/scripts --force /usr/local/etc/scripts && source $CONFIG_DIR/config.sh"
+alias update-conf="svn export https://github.com/theleos88/vpp-bench/trunk/scripts --force /usr/local/etc/scripts && source $CONFIG_DIR/config.sh"
 alias show-conf="cat $CONFIG_DIR/config.sh"
-alias list-scripts="ls $CONFIG_DIR"
+alias list-scripts="ls -l $CONFIG_DIR/*.sh"
 alias dpdk-setup="$RTE_SDK/usertools/dpdk-setup.sh"
 alias vppctl="sudo $SFLAG vppctl"
