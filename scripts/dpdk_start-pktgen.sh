c7c03b9a58e636b431c0420099a837e6253e8a18
Leonardo Linguaglossa <theleos88@gmail.com>
2017-04-04 17:29:06 +0200

    Dpdk, support for custom packet generator script

--- scripts/dpdk_start-pktgen.sh
+++ scripts/dpdk_start-pktgen.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
+PKTGEN_SCRIPT=$CONFIG_DIR/lua_forwarding-rate.lua
+
 cd $RTE_PKTGEN
+
+echo "***************************"
 echo "Using pkt_gen: $RTE_PKTGEN"
+echo "Pktgen-script: $PKTGEN_SCRIPT"
+echo "***************************"
+
 ERRFILE=/tmp/err.out
 
 if [[ $# -eq 0 ]] ; then
     echo 'STARTING PKTGEN WITH DEFAULT PARAMETERS' 
     echo '(cores: 12-16; [LC0P0, port0, rx] [LC0P1, port1, tx] ).' 
-    F=`$CONFIG_DIR/pktgenconf.sh 200`
-	sudo app/app/x86_64-native-linuxapp-gcc/pktgen -l 12,13-16 -n 1 -w $LC0P0 -w $LC0P1 -- -P -T -m "[13-14].0,[15-16].1" -f $F
+
+	sudo app/app/x86_64-native-linuxapp-gcc/pktgen -l 12,13-16 -n 1 -w $LC0P0 -w $LC0P1 -- -P -T -m "[13-14].0,[15-16].1"
 	exit 1
 fi
 
 if [ "$1" == "forwarding" ]; then
-	sudo app/app/x86_64-native-linuxapp-gcc/pktgen -l 12,13-16 -n 1 -w $LC0P0 -w $LC0P1 -- -P -T -m "[13-14].0,[15-16].1" -f $CONFIG_DIR/lua_forwarding-rate.lua
+	sudo app/app/x86_64-native-linuxapp-gcc/pktgen -l 12,13-16 -n 1 -w $LC0P0 -w $LC0P1 -- -P -T -m "[13-14].0,[15-16].1" -f $PKTGEN_SCRIPT
 else
 	echo "Error: Not yet patched"
 	######## NOT PATCHED! 03/04/2017, Leos ###########
