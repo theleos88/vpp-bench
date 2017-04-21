b9c935a19fdb715332364d0462ee79b8e10c893d
Leonardo Linguaglossa <theleos88@gmail.com>
2017-04-21 18:36:24 +0200

    Promiscuous mode + 32 bit prefix

--- scripts/vpp_set-linecards-address.sh
+++ scripts/vpp_set-linecards-address.sh
@@ -6,14 +6,20 @@ if [[ $# -eq 0 ]] ; then
     echo 'linecard.sh [list of linecards to set]'
 
     #Setting IP
-	sudo $SFLAG vppctl -p vpp set int ip address $NAMELC1P0 $IPLC1P0/24
-	sudo $SFLAG vppctl -p vpp set int ip address $NAMELC1P1 $IPLC1P1/24
+	sudo $SFLAG $BINS/vppctl -p vpp set int ip address $NAMELC1P0 $IPLC1P0/32
+	sudo $SFLAG $BINS/vppctl -p vpp set int ip address $NAMELC1P1 $IPLC1P1/32
 
 	# Setting ARPs
-	sudo $SFLAG vppctl -p vpp set ip arp static $NAMELC1P0 $IPLC0P0 $MACLC0P0
+	sudo $SFLAG $BINS/vppctl -p vpp set ip arp static $NAMELC1P0 $IPLC0P0 $MACLC0P0
 
-	sudo $SFLAG vppctl -p vpp set interface state $NAMELC1P0 up
-	sudo $SFLAG vppctl -p vpp set interface state $NAMELC1P1 up
+	sudo $SFLAG $BINS/vppctl -p vpp set interface state $NAMELC1P0 up
+	sudo $SFLAG $BINS/vppctl -p vpp set interface state $NAMELC1P1 up
+
+    sudo $SFLAG $BINS/vppctl -p vpp set interface promiscuous on $NAMELC1P0
+    sudo $SFLAG $BINS/vppctl -p vpp set interface promiscuous on $NAMELC1P1
+
+    sudo $SFLAG vppctl -p vpp1 set interface promiscuous on $NAMELC1P1
+    sudo $SFLAG vppctl -p vpp1 set interface promiscuous on $NAMELC1P0
 
     exit 1
 fi
