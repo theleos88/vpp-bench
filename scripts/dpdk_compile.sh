e7e53c2851d26e9c295805bff42e580a0c0b3139
Leonardo Linguaglossa <theleos88@gmail.com>
2017-05-18 15:26:36 +0200

    Dpdk compile

---
+++ scripts/dpdk_compile.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This script simply recompiles all DPDK
+
+cd $RTE_SDK
+
+if [ -z "$RTE_TARGET" ]; then
+	echo "Please, define RTE_TARGET";
+else
+	make config T=$RTE_TARGET
+	make T=$RTE_TARGET
+fi
