5ad68524cd85c603c92ef005fce1eac981305225
Leonardo Linguaglossa <theleos88@gmail.com>
2017-06-30 19:07:48 +0200

    DPDK start with a custom .pkt file

--- scripts/dpdk_start-pktgen.sh
+++ scripts/dpdk_start-pktgen.sh
@@ -22,8 +22,8 @@ fi
 if [ "$1" == "forwarding" ]; then
 	sudo app/app/x86_64-native-linuxapp-gcc/pktgen -l 12,13-16 -n 1 -w $LC0P0 -w $LC0P1 -- -P -T -m "[13-14].0,[15-16].1" -f $PKTGEN_SCRIPT
 else
-	echo "Error: Not yet patched"
+	echo "Starting pktgen with a .pkt file"
 	######## NOT PATCHED! 03/04/2017, Leos ###########
     #F=`$CONFIG_DIR/dpdk_config-pktgen.sh $1`
-	#sudo app/app/x86_64-native-linuxapp-gcc/pktgen -l 12,13-16 -n 1 -w $LC0P0 -w $LC0P1 -- -P -T -m "[13-14].0,[15-16].1" -f $F
+	sudo app/app/x86_64-native-linuxapp-gcc/pktgen -l 12,13-16 -n 1 -w $LC0P0 -w $LC0P1 -- -P -T -m "[13-14].0,[15-16].1" -f $1
 fi
