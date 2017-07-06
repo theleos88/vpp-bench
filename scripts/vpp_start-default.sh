85c01e9a28fd9841515081ff2a1de8ed8080474a
Leonardo Linguaglossa <theleos88@gmail.com>
2017-07-06 15:22:37 +0200

    Start default now modifies the startup.conf file

--- scripts/vpp_start-default.sh
+++ scripts/vpp_start-default.sh
@@ -7,5 +7,12 @@ echo "VPP_ROOT in : $VPP_ROOT"
 echo "Binairies in : $BINS"
 echo "Plugins in : $PLUGS"
 
-echo 'STARTING WITH DEFAULT PARAMETERS (name=vpp).'
+if [[ $# -eq 1 ]] ; then
+    echo "Changing Prefix with: $1"
+	sed -i "s/^\(prefix \).*/\1$1/" $STARTUP_CONF
+fi
+
+PREFIX=`cat $STARTUP_CONF | grep prefix | awk '{print $2}' | xargs echo -n`
+
+echo "STARTING VPP WITH (name=$PREFIX)."
 sudo $BINS/vpp `cat $STARTUP_CONF` plugin_path $PLUGS
