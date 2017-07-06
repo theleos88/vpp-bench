751d69e04d9ffa4ff5cf6e1532087fde1acbdddc
Leonardo Linguaglossa <theleos88@gmail.com>
2017-07-06 15:09:23 +0200

    Taking prefix from the real Startup.conf

--- scripts/vpp_set-linecards-address.sh
+++ scripts/vpp_set-linecards-address.sh
@@ -2,26 +2,39 @@
 
 DEFAULTIP="99.99.99.99"
 
+PREFIX=`cat $STARTUP_CONF | grep prefix | awk '{print $2}' | xargs echo -n`
+
+
 if [[ $# -eq 0 ]] ; then
     echo 'No parameters provided. Running in default mode:'
     echo 'linecard.sh [list of linecards to set]'
+    echo Prefix:$PREFIX
 
 
 	# Setting ARPs
-	sudo $SFLAG $BINS/vppctl -p vpp set ip arp static $NAMELC1P0 $DEFAULTIP $MACLC0P0
-    sudo $SFLAG $BINS/vppctl -p vpp ip route add 0.0.0.0/0 via $DEFAULTIP
+	sudo $SFLAG $BINS/vppctl -p $PREFIX set ip arp static $NAMELC1P0 $DEFAULTIP $MACLC0P0
+    sleep 0.1
+    sudo $SFLAG $BINS/vppctl -p $PREFIX ip route add 0.0.0.0/0 via $DEFAULTIP
 
-    sudo $SFLAG $BINS/vppctl -p vpp set interface promiscuous on $NAMELC1P1
-    sudo $SFLAG $BINS/vppctl -p vpp set interface promiscuous on $NAMELC1P0
+    sleep 0.1
+
+    sudo $SFLAG $BINS/vppctl -p $PREFIX set interface promiscuous on $NAMELC1P1
+    sleep 0.1
+    sudo $SFLAG $BINS/vppctl -p $PREFIX set interface promiscuous on $NAMELC1P0
+
+    sleep 0.1
 
     #Setting IP
-	sudo $SFLAG $BINS/vppctl -p vpp set int ip address $NAMELC1P1 $IPLC1P1/32
-	sudo $SFLAG $BINS/vppctl -p vpp set int ip address $NAMELC1P0 $IPLC1P0/32
+	sudo $SFLAG $BINS/vppctl -p $PREFIX set int ip address $NAMELC1P1 $IPLC1P1/32
+    sleep 0.1
+	sudo $SFLAG $BINS/vppctl -p $PREFIX set int ip address $NAMELC1P0 $IPLC1P0/32
 
+    sleep 0.1
 
-	sudo $SFLAG $BINS/vppctl -p vpp set interface state $NAMELC0P0 up
-	sudo $SFLAG $BINS/vppctl -p vpp set interface state $NAMELC0P1 up
-
+	sudo $SFLAG $BINS/vppctl -p $PREFIX set interface state $NAMELC1P1 up
+    sleep 0.1
+	sudo $SFLAG $BINS/vppctl -p $PREFIX set interface state $NAMELC1P0 up
+    sleep 0.1
 
     exit 1
 fi
@@ -31,4 +44,4 @@ echo "Configuring Linecards for IP mode"
 # Optional: add ip addresses to check ping
 
 # Show for sanity check
-#vppctl -p vpp show int address
+#vppctl -p $PREFIX show int address
