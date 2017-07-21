48f0735c7b8910303a66927f60d67f4f7d91b8d1
Leonardo Linguaglossa <theleos88@gmail.com>
2017-07-21 15:35:34 +0200

    Support for IP6 in add table and setup interfaces

--- scripts/config.sh
+++ scripts/config.sh
@@ -54,6 +54,10 @@ export IP6LC1P1="2011:3::ffff"
 export IPLOOP="10.0.0.0"
 export IP6LOOP="2011:1::ffff"
 
+# Default routes
+DEFAULTIP="99.99.99.99"
+DEFAULTIP6="2211:2::ffff"
+
 # VPP
 #export VPP_ROOT=/usr/local/src/vpp
 export VPP_ROOT=/home/leos/vppdev/vpp
