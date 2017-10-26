69fd5336604e7856e7ef5a22e03a7e9d5d5c1166
theleos <theleos88@gmail.com>
2017-10-26 09:59:32 +0100

    Dpdk_start pktgen considers now the tgdpdk.conf

--- scripts/dpdk_start-pktgen.sh
+++ scripts/dpdk_start-pktgen.sh
@@ -1,29 +1,46 @@
 #!/bin/bash
 
 PKTGEN_SCRIPT=$CONFIG_DIR/lua_forwarding-rate.lua
+: ${DPDK_CONF:=$CONFIG_DIR/tgdpdk.conf}
+
+##########################
+declare -A param
+param["prefix"]="pippo"
+param["main-core"]="1"
+param["workers"]="3-4"
+param["dev"]=""
+param["map"]=""
+#########################
 
 cd $RTE_PKTGEN
 
 echo "***************************"
 echo "Using pkt_gen: $RTE_PKTGEN"
+echo "Config-script: $DPDK_CONF"
 echo "Pktgen-script: $PKTGEN_SCRIPT"
 echo "***************************"
 
 ERRFILE=/tmp/err.out
 
-if [[ $# -eq 0 ]] ; then
-    echo 'STARTING PKTGEN WITH DEFAULT PARAMETERS' 
-    echo '(cores: 12-16; [LC1P0, port0, rx] [LC1P1, port1, tx] ).' 
+for i in "${!param[@]}"; do param["$i"]=$(echo -e `grep $i $DPDK_CONF | cut -d '=' -f 2-`); done
 
-	sudo app/app/x86_64-native-linuxapp-gcc/pktgen -l 12,13-16 -n 1 -w $LC0P0 -w $LC0P1 -- -P -T -m "[13-14].0,[15-16].1"
-	exit 1
+
+if [[ $# -eq 0 ]] ; then
+  echo 'STARTING PKTGEN WITH DEFAULT PARAMETERS' 
+
+  sudo -E app/app/x86_64-native-linuxapp-gcc/pktgen -l ${param['main-core']},${param['workers']} -n 1 -m 2048 ${param['dev']} -- -P -T -m ${param['map']}
+  #sudo app/app/x86_64-native-linuxapp-gcc/pktgen -l 12,13-16 -n 1 -w $LC0P0 -w $LC0P1 -- -P -T -m "[13-14].0,[15-16].1"
+exit 1
 fi
 
 if [ "$1" == "forwarding" ]; then
-	sudo app/app/x86_64-native-linuxapp-gcc/pktgen -l 1,2-5 -n 1 -w $LC0P0 -w $LC0P1 -- -P -T -m "[2-3].0,[4-5].1" -f $PKTGEN_SCRIPT
+	echo "NOT PATCHED"
+	#sudo app/app/x86_64-native-linuxapp-gcc/pktgen -l 1,2-5 -n 1 -w $LC0P0 -w $LC0P1 -- -P -T -m "[2-3].0,[4-5].1" -f $PKTGEN_SCRIPT
+
 else
-	echo "Starting pktgen with a .pkt file"
+	echo "NOT PATCHED"
+	#echo "Starting pktgen with a .pkt file"
 	######## NOT PATCHED! 03/04/2017, Leos ###########
     #F=`$CONFIG_DIR/dpdk_config-pktgen.sh $1`
-	sudo app/app/x86_64-native-linuxapp-gcc/pktgen -l 12,13-16 -n 1 -w $LC0P0 -w $LC0P1 -- -P -T -m "[13-14].0,[15-16].1" -f $1
+	#sudo app/app/x86_64-native-linuxapp-gcc/pktgen -l 12,13-16 -n 1 -w $LC0P0 -w $LC0P1 -- -P -T -m "[13-14].0,[15-16].1" -f $1
 fi
