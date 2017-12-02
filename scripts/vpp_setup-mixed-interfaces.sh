65208d4d1b7a967764fc2a7417219c61d68cc7f0
Leonardo Linguaglossa <theleos88@gmail.com>
2017-12-02 17:47:19 +0100

    Fixed bug in Mixed interfaces

--- scripts/vpp_setup-mixed-interfaces.sh
+++ scripts/vpp_setup-mixed-interfaces.sh
@@ -10,8 +10,8 @@ echo 'vpp_setup-mixed-interfaces.sh'
 echo "Prefix:$PREFIX"
 
 # Setting ARPs
-sudo $SFLAG $BINS/vppctl -p $PREFIX set ip arp static $NAMELC0P0 $DEFAULTIP $MACLC1P0
-sudo $SFLAG $BINS/vppctl -p $PREFIX ip route add 0.0.0.0/0 via $DEFAULTIP
+sudo $SFLAG $BINS/vppctl -p $PREFIX set ip arp static $NAMELC0P0 $IPLC1P0 $MACLC1P0	#Leonardo: using Sal's config
+#sudo $SFLAG $BINS/vppctl -p $PREFIX ip route add 0.0.0.0/0 via $DEFAULTIP	# Leonardo: no default route for mixed interfaces!!!
 sleep 0.1
 
 # Promiscuous mode on. Optional
@@ -23,8 +23,9 @@ sleep 0.1
 # Setting IP
 echo "IPV4"
 
-sudo $SFLAG $BINS/vppctl -p $PREFIX set int ip address $NAMELC0P1 $IPLC0P1/24
-sudo $SFLAG $BINS/vppctl -p $PREFIX set int ip address $NAMELC0P0 $IPLC0P0/24	#/24 Since this is for the bridge
+#sudo $SFLAG $BINS/vppctl -p $PREFIX set int ip address $NAMELC0P1 $IPLC0P1/24	# Leonardo: no addresses here for mixed!!!
+sudo $SFLAG $BINS/vppctl -p $PREFIX set int ip address $NAMELC0P0 $IPLC0P0/24	# Leonardo: two addresses on the IFx where we go out.
+sudo $SFLAG $BINS/vppctl -p $PREFIX set int ip address $NAMELC0P0 192.168.2.0/24	#/24 Since this is for the bridge
 
 sleep 0.1
 
@@ -64,7 +65,7 @@ sudo $SFLAG $BINS/vppctl -p $PREFIX ip route add 10.0.1.0/28 via $IPLC0P1
 sudo $SFLAG $BINS/vppctl -p $PREFIX set ip arp static $NAMELC0P0 192.168.2.2 $MACLC1P0
 sudo $SFLAG $BINS/vppctl -p $PREFIX set ip arp static $NAMELC0P1 192.168.2.4 $MACLC1P1
 sudo $SFLAG $BINS/vppctl -p $PREFIX set ip arp static $NAMELC0P1 $IPLC0P0 90:e2:ba:cb:f5:48
-sudo $SFLAG $BINS/vppctl -p $PREFIX set ip arp static $NAMELC0P1 $IPLC1P1 90:e2:ba:cb:f5:49
+sudo $SFLAG $BINS/vppctl -p $PREFIX set ip arp static $NAMELC0P1 $IPLC0P1 90:e2:ba:cb:f5:49	#Leonardo: found a bug!!!
 
 echo "Status up"
 sudo $SFLAG $BINS/vppctl -p $PREFIX set interface state $NAMELC0P1 up
