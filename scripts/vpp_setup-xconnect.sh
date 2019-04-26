eb69adb2b38f33b64f7f6f2e75c067baba7ed3d5
Leonardo Linguaglossa <theleos88@gmail.com>
2019-04-26 14:10:24 +0200

    L. xconnect bidirectional and using -s

--- scripts/vpp_setup-xconnect.sh
+++ scripts/vpp_setup-xconnect.sh
@@ -7,13 +7,23 @@ cd $VPP_ROOT
 BINS="$VPP_ROOT/build-root/install-vpp-native/vpp/bin"
 PLUGS="$VPP_ROOT/build-root/install-vpp-native/vpp/lib64/vpp_plugins"
 SFLAG="env PATH=$PATH:$BINS"
-PREFIX=`cat $STARTUP_CONF | grep prefix | awk '{print $2}' | xargs echo -n`
+#PREFIX=`cat $STARTUP_CONF | grep prefix | awk '{print $2}' | xargs echo -n`
+PREFIX=`cat $STARTUP_CONF | grep cli-listen | awk '{print $2}' | xargs echo -n`
 
 if [[ $# -eq 0 ]] ; then
-    echo 'STARTING WITH DEFAULT PARAMETERS (name=vpp; LC0P1->LC0P0). For next Usage:'
-    sudo $SFLAG $BINS/vppctl -p $PREFIX set int l2 xconnect $NAMELC0P1 $NAMELC0P0
-    sudo $SFLAG $BINS/vppctl -p $PREFIX set int state $NAMELC0P0 up
-    sudo $SFLAG $BINS/vppctl -p $PREFIX set int state $NAMELC0P1 up
+    echo 'STARTING WITH DEFAULT PARAMETERS (Bidirectional LC0P1<->LC0P0). For next Usage:'
+
+    if [[ $EUID -ne 0 ]]; then
+      sudo $SFLAG $BINS/vppctl -s $PREFIX set int l2 xconnect $NAMELC0P1 $NAMELC0P0
+      sudo $SFLAG $BINS/vppctl -s $PREFIX set int l2 xconnect $NAMELC0P0 $NAMELC0P1
+      sudo $SFLAG $BINS/vppctl -s $PREFIX set int state $NAMELC0P0 up
+      sudo $SFLAG $BINS/vppctl -s $PREFIX set int state $NAMELC0P1 up
+    else
+      $BINS/vppctl -s $PREFIX set int l2 xconnect $NAMELC0P1 $NAMELC0P0
+      $BINS/vppctl -s $PREFIX set int l2 xconnect $NAMELC0P0 $NAMELC0P1
+      $BINS/vppctl -s $PREFIX set int state $NAMELC0P0 up
+      $BINS/vppctl -s $PREFIX set int state $NAMELC0P1 up
+    fi
 
 	echo "Done Xconnecting"
     exit 1
