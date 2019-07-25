7a48eb16f5ebe424bd8593aeaecd83aca36c5188
Leonardo Linguaglossa <theleos88@gmail.com>
2019-07-25 17:27:55 +0200

    New compiling method

--- scripts/vpp_compile.sh
+++ scripts/vpp_compile.sh
@@ -3,5 +3,10 @@
 # This script simply recompiles all VPP
 
 cd $VPP_ROOT
-#make wipe-release
+make wipe-release
+rm -r build-root/.ccache/
+rm -r build-root/install-vpp-native
+rm -r build-root/build-vpp-native/
+
+make install-ext-deps
 make build-release
