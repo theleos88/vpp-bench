3345db8ff0811c6e0cf28391de5734cb51b38581
Leonardo Linguaglossa <theleos88@gmail.com>
2019-09-06 17:11:01 +0200

    New api for getting measurements

--- scripts/vpp_loop-eventlogger.sh
+++ scripts/vpp_loop-eventlogger.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 runs.sh "event-logger stop"
 
+LOG="/tmp/log_test_vpp_elog.log"
+
 for i in `seq 1 6`; do
 	sleep 1
     sudo kill -USR1 `pidof vpp`
 	sleep 2
+
 	sudo mv /tmp/vpplog /tmp/result.$1.$2.r$3.dat.$i
+	if [ $? -eq 0 ]; then
+   	 	echo "$1.$2.r$3 : OK" >> $LOG
+	else
+   	 	echo "$1.$2.r$3 : FAIL" >> $LOG
+	fi
+
 	sleep 1
 
 #    a='event-logger save'
