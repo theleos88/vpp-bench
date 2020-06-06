a47744324faca64be4f340db11f258db1e73f911
Leonardo Linguaglossa <theleos88@gmail.com>
2020-06-06 20:59:30 +0000

    L. update to 19 for ip4 ip6

--- scripts/vpp_setup-ip4-ip6.sh
+++ scripts/vpp_setup-ip4-ip6.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PREFIX=`cat $STARTUP_CONF | grep prefix | awk '{print $2}' | xargs echo -n`
+PREFIX=`cat $STARTUP_CONF | grep cli-listen | awk '{print $2}' | xargs echo -n`
 DEFAULTIP="99.99.99.99"
 DEFAULTIP6="2211:2::ffff"
 
@@ -9,36 +9,36 @@ echo 'vpp_setup-mixed-interfaces.sh'
 echo "Prefix:$PREFIX"
 
 # Setting ARPs
-sudo $SFLAG $BINS/vppctl -p $PREFIX set ip arp static $NAMELC1P0 $DEFAULTIP $MACLC0P0
+sudo $SFLAG $BINS/vppctl -s $PREFIX set ip arp static $NAMELC1P0 $DEFAULTIP $MACLC0P0
 sleep 0.1
-sudo $SFLAG $BINS/vppctl -p $PREFIX ip route add 0.0.0.0/0 via $DEFAULTIP
+sudo $SFLAG $BINS/vppctl -s $PREFIX ip route add 0.0.0.0/0 via $DEFAULTIP
 
 # Promiscuous mode on. Optional
-sudo $SFLAG $BINS/vppctl -p $PREFIX set interface promiscuous on $NAMELC1P1
+sudo $SFLAG $BINS/vppctl -s $PREFIX set interface promiscuous on $NAMELC1P1
 sleep 0.1
-sudo $SFLAG $BINS/vppctl -p $PREFIX set interface promiscuous on $NAMELC1P0
+sudo $SFLAG $BINS/vppctl -s $PREFIX set interface promiscuous on $NAMELC1P0
 
 sleep 0.1
 
 # Setting IP
 echo "IPV6"
 
-sudo $SFLAG $BINS/vppctl -p $PREFIX set int ip address $NAMELC1P1 $IPLC1P1/32
+sudo $SFLAG $BINS/vppctl -s $PREFIX set int ip address $NAMELC1P1 $IPLC1P1/32
 sleep 0.1
-sudo $SFLAG $BINS/vppctl -p $PREFIX set int ip address $NAMELC1P0 $IPLC1P0/32
+sudo $SFLAG $BINS/vppctl -s $PREFIX set int ip address $NAMELC1P0 $IPLC1P0/32
 
 sleep 0.1
 
 ##IPV6
 echo "IPV6"
 
-sudo $SFLAG $BINS/vppctl -p $PREFIX set interface ip address $NAMELC1P0 $IP6LC1P0/128
-sudo $SFLAG $BINS/vppctl -p $PREFIX set interface ip address $NAMELC1P1 $IP6LC1P1/128
-sudo $SFLAG $BINS/vppctl -p $PREFIX ip route add ::/0 via $DEFAULTIP6 $NAMELC1P0
-sudo $SFLAG $BINS/vppctl -p $PREFIX set ip6 neighbor $NAMELC1P0 $DEFAULTIP6 $MACLC1P1 static
+sudo $SFLAG $BINS/vppctl -s $PREFIX set interface ip address $NAMELC1P0 $IP6LC1P0/128
+sudo $SFLAG $BINS/vppctl -s $PREFIX set interface ip address $NAMELC1P1 $IP6LC1P1/128
+sudo $SFLAG $BINS/vppctl -s $PREFIX ip route add ::/0 via $DEFAULTIP6 $NAMELC1P0
+sudo $SFLAG $BINS/vppctl -s $PREFIX set ip6 neighbor $NAMELC1P0 $DEFAULTIP6 $MACLC1P1 static
 
 ###
 echo "Status up"
 
-sudo $SFLAG $BINS/vppctl -p $PREFIX set interface state $NAMELC1P1 up
-sudo $SFLAG $BINS/vppctl -p $PREFIX set interface state $NAMELC1P0 up
+sudo $SFLAG $BINS/vppctl -s $PREFIX set interface state $NAMELC1P1 up
+sudo $SFLAG $BINS/vppctl -s $PREFIX set interface state $NAMELC1P0 up
